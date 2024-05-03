e8384eaed55ca23ec9846f638eb84bcc3e1e7311
Han Kyuhyun <khh5345@naver.com>
2024-05-03 15:12:50 +0900

    게시판, 댓글 구현 완료
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

---
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
+
